6a9633bf132c9290975a1fd3a266dad95a596f7e
Peter Stuifzand <peter.stuifzand@gmail.com>
2011-10-24 23:53:44 +0200

    Added templates

--- lib/Weblog.pm
+++ lib/Weblog.pm
@@ -1,77 +1,50 @@
 package Weblog;
 use parent 'Plack::Component';
+
 use strict;
 use warnings;
+
 use Data::Dumper;
 use Weblog::DB;
 
 use YAML::XS 'LoadFile';
 use Plack::Util::Accessor qw/config/;
 use Date::Period::Human;
+use Template;
 
 sub call {
     my $self = shift;
     my $env = shift;
 
+    if ($env->{PATH_INFO} =~ m{^/code}) {
+        return [ 302, [ 'Location', 'http://github.com/pstuifzand/plack-weblog' ], [] ];
+    }
+
     my $title = $self->config->{weblog}{title};
     my $db = Weblog::DB->Connect(%{$self->config->{db}{weblog}});
 
     my $dph = Date::Period::Human->new();
+    my $template = Template->new(INCLUDE_PATH => './share');
 
-    if ($env->{PATH_INFO} =~ m{^/code}) {
-        return [ 302, [ 'Location', 'http://github.com/pstuifzand/plack-weblog' ], [] ];
-    }
-    elsif ($env->{PATH_INFO} =~ m{^/post/([a-z\-]+)$}) {
+    my $out = '';
+
+    if ($env->{PATH_INFO} =~ m{^/post/([a-z\-]+)$}) {
         my $slug = $1;
-        my @sb;
-        push @sb, "<DOCTYPE html><html><head><title>$title</title><link rel='stylesheet' href='/public/bootstrap.min.css' /><link rel='stylesheet' href='/public/style.css' /></head><body>\n";
-        push @sb, "<div class='container'>";
-        push @sb, "<h1><a href='/'>",$title,"</a></h1>\n";
-
         my $entry = $db->Entry($slug);
-            push @sb, qq{<div class="entry">};
-            push @sb, "<h2>",$entry->{title},"</h2>\n";
-            push @sb, "<div class='content'>", $entry->{content}, "</div>", "\n";
-            push @sb, "<div class='tools'>";
-            push @sb, "  <span class='tool'><a href='/post/$entry->{slug}#comments'>Reageer</a></span> &middot; ";
-            push @sb, "  <span class='created'><a href='/post/$entry->{slug}'>",$dph->human_readable($entry->{created}),"</a></span>";
-            push @sb, "</div>\n";
-            push @sb, qq{</div>};
-        push @sb, qq{</div>};
-        push @sb, "</body></html>\n";
-
-        return [ 200, [ 'Content-Type', 'text/html' ], [ @sb ] ];
+        $template->process('entry.tp', { title => $title, human_readable_date => sub { $dph->human_readable($_[0]) }, entry => $entry }, \$out) or die $Template::ERROR;
     }
     else {
         my @entries = $db->Entries;
-
-        my @sb;
-        push @sb, "<DOCTYPE html><html><head><title>$title</title><link rel='stylesheet' href='/public/bootstrap.min.css' /><link rel='stylesheet' href='/public/style.css' /></head><body>\n";
-        push @sb, "<div class='container'>";
-        push @sb, "<h1>",$title,"</h1>\n";
-
-        push @sb, "<div class='row'>";
-        push @sb, "<div class='span11'>";
         for my $entry (@entries) {
-            push @sb, qq{<div class="entry">};
-            push @sb, "<h2><a href='/post/$entry->{slug}'>",$entry->{title},"</a></h2>\n";
-            push @sb, "<div class='content'>", $entry->{content}, "</div>", "\n";
-            push @sb, "<div class='tools'>";
-            push @sb, "  <span class='tool'><a href='/post/$entry->{slug}#comments'>Reageer</a></span> &middot; ";
-            push @sb, "  <span class='created'><a href='/post/$entry->{slug}'>",$dph->human_readable($entry->{created}),"</a></span>";
-            push @sb, "</div>\n";
-            push @sb, qq{</div>};
+            $template->process('entry.tp', { title => $title, human_readable_date => sub { $dph->human_readable($_[0]) }, entry => $entry }, \$out) or die $Template::ERROR;
         }
-        push @sb, "</div>";
-        push @sb, "<div class='span5'>";
-        push @sb, q{<p>Stuur mij een e-mail: <a href="mailto:peter@tweevijftig.nl">peter@tweevijftig.nl</a></p>};
-        push @sb, q{<p><a href="/code">Source code</a></p>};
-        push @sb, qq{</div>};
-        push @sb, qq{</div>};
-        push @sb, "</body></html>\n";
-
-        return [ 200, [ 'Content-Type', 'text/html' ], [ @sb ] ];
     }
+
+    my $out2 = '';
+    $template->process('layout.tp', { title => $title, insert_content_here => $out }, \$out2) or die $Template::ERROR;
+
+
+    return [ 200, [ 'Content-Type', 'text/html' ], [ $out2 ] ];
 }
 
 1;
