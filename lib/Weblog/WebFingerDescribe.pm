6553bcf906b0cb868ea2ad4024223de431bcd740
Peter Stuifzand <peter@stuifzand.eu>
2012-04-24 14:09:15 +0200

    Use HTTP_HOST instead of static name

--- lib/Weblog/WebFingerDescribe.pm
+++ lib/Weblog/WebFingerDescribe.pm
@@ -13,17 +13,18 @@ sub call {
 
     my $req = Plack::Request->new($env);
     my $uri = $req->param('uri');
+    my $host = $env->{HTTP_HOST};
 
     $uri =~ s/^acct://;
 
-    if ($uri ne 'peter@tweevijftig.nl') {
+    if ($uri ne 'peter@'.$host) {
         return $req->new_response(404, [], ['Not Found'])->finalize;
     }
 
     my $out = <<"XML";
 <?xml version='1.0' encoding='UTF-8'?>
 <XRD xmlns='http://docs.oasis-open.org/ns/xri/xrd-1.0'>
-<Subject>acct:peter\@tweevijftig.nl</Subject>
+<Subject>acct:peter\@$host</Subject>
 <Link rel='http://portablecontacts.net/spec/1.0#me' href='http://peterstuifzand.com/contact.json' />
 <Link rel='http://webfinger.net/rel/profile-page' href='http://peterstuifzand.com' type='text/html' />
 <Link rel='http://webfinger.net/rel/user-photo' href='http://peterstuifzand.com/nieuw_profiel_cropped_300.jpg' type='image/jpeg' />
